bd3f7568c34509996d42e856547dd56328021f3d
Peter Kvale <pjkvale@gmail.com>
2021-02-01 06:08:48 -0500

    This is the initial creation of the eclipse project for the shortcut generator.  This is adding a more or less blank main class file with an outline of comments generated for the project layout

---
+++ ShortcutGenerator/src/module-info.java
@@ -0,0 +1,48 @@
+/**
+ * This program is creating a searchable list of shortcuts based on a text file.  The text file is formatted in the following 
+ * format: -shortcut: #searchTerm1, #searchTerm2, #searchTerm3, #searchTerm4, etc., *Description
+ * Example: (eclipse) -Ctrl+S: #save
+ */
+/**
+ * @author peter kvale
+ * 1/28/21
+ */
+module shortcutGenerator {
+	/*
+	 * import text file
+	 */
+
+	/*
+	 * create hash table
+	 * 	-creates a hash table of objects with names as shortcuts and with properties as the search terms and descriptions
+	 */
+
+	/*
+	 * Start GUI 
+	 * 	-user input box
+	 * 	-output popup
+	 * 	-10 recent searches
+	 * 	-save searches
+	 * 	-load searches
+	 */
+
+	/*
+	 * declare search function 
+	 * findSearchedShortcuts array[objects](hashtable myHashtable){
+	 * }
+	 */
+	
+	/*
+	 * declare search function 
+	 * getSearchedDescriptions array[objects](hashtable myHashtable){
+	 * }
+	 */
+
+	/*
+	 * declare load function 
+	 */
+	
+	/*
+	 * declare save function 
+	 */
+}
